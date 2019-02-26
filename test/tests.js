3262f4f5d4824ef4795a7fa3d8d0fccdbd8d64b4
sunilmundru <sunilmundru@gmail.com>
2019-02-25 21:06:17 -0600

    initial test

--- test/tests.js
+++ test/tests.js
@@ -4,8 +4,8 @@ QUnit.test('Testing area function', function (assert) {
     assert.equal(findarea(-3,4), 0, 'with negative base');
     assert.equal(findarea(6,-3), 0, 'with negative height');
     assert.equal(findarea(), 0, 'with no paramaters');
-    assert.equal(findarea(0,8), 0, 'base zero');
-    assert.equal(findarea(7,0), 0, 'height zero');
+    assert.equal(findarea(0,8), 0, 'With base zero');
+    assert.equal(findarea(7,0), 0, 'With height zero');
 });
 
 
